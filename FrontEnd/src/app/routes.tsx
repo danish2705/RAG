7f531f4b853ec336334c786d72be00da0de0a679
danish2705 <danishmeraj06@gmail.com>
2026-07-08 15:40:34 +0530

    fixed the devaition issue

--- FrontEnd/src/app/routes.tsx
+++ FrontEnd/src/app/routes.tsx
@@ -2,20 +2,20 @@ import { createBrowserRouter } from "react-router";
 import { Layout } from "./components/layout/Layout";
 import { Dashboard } from "./pages/Dashboard";
 import { NewDeviation } from "./pages/InputQuery";
-import { AIRecommendation } from "./pages/AIRecommendation";
-import { ImpactAssessment } from "./pages/ImpactAssessment";
-import { RootCause } from "./pages/RootCause";
-import { Capa } from "./pages/Capa";
-import { Summary } from "./pages/Summary";
+import { AIRecommendation } from "./pages/deviation/AIRecommendation";
+import { ImpactAssessment } from "./pages/deviation/ImpactAssessment";
+import { RootCause } from "./pages/deviation/RootCause";
+import { Capa } from "./pages/deviation/Capa";
+import { Summary } from "./pages/deviation/Summary";
 import { Records } from "./pages/Records";
 import { AuditTrail } from "./pages/AuditTrail";
 import { Reports } from "./pages/Reports";
 import { Settings } from "./pages/Settings";
-import { ChangeImpactAssessment } from "./pages/ChangeImpactAssessment";
-import { RiskCriticality } from "./pages/RiskCriticality";
-import { ValidationTesting } from "./pages/ValidationTesting";
+import { ChangeImpactAssessment } from "./pages/changeControl/ChangeImpactAssessment";
+import { RiskCriticality } from "./pages/changeControl/RiskCriticality";
+import { ValidationTesting } from "./pages/changeControl/ValidationTesting";
 import { ImplementationControl } from "./pages/ImplementationControl";
-import { ChangecontrolSummary } from "./pages/ChangecontrolSummary";
+import { ChangecontrolSummary } from "./pages/changeControl/ChangecontrolSummary";
 
 export const router = createBrowserRouter([
   {
@@ -29,10 +29,19 @@ export const router = createBrowserRouter([
       { path: "deviation/root-cause", Component: RootCause },
       { path: "deviation/capa", Component: Capa },
       { path: "deviation/summary", Component: Summary },
-      { path: "change-control/change-impact-assessment", Component: ChangeImpactAssessment },
+      {
+        path: "change-control/change-impact-assessment",
+        Component: ChangeImpactAssessment,
+      },
       { path: "change-control/risk-criticality", Component: RiskCriticality },
-      { path: "change-control/validation-testing", Component: ValidationTesting },
-      { path: "change-control/implementation", Component: ImplementationControl },
+      {
+        path: "change-control/validation-testing",
+        Component: ValidationTesting,
+      },
+      {
+        path: "change-control/implementation",
+        Component: ImplementationControl,
+      },
       { path: "change-control/summary", Component: ChangecontrolSummary },
       { path: "records", Component: Records },
       { path: "audit-trail", Component: AuditTrail },

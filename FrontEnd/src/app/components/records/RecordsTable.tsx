d94af1636aaa9a296b7391136dac8f8eaf6cb4c8
saimanikantaballa14 <saimanikanta.balla@ascendion.com>
2026-07-21 18:30:30 +0530

    Updated Width - Query

--- FrontEnd/src/app/components/records/RecordsTable.tsx
+++ FrontEnd/src/app/components/records/RecordsTable.tsx
@@ -83,7 +83,7 @@ export const RecordsTable: React.FC<RecordsTableProps> = ({
                   <ArrowUpDown className="h-3.5 w-3.5 text-muted-foreground" />
                 </button>
               </TableHead>
-              <TableHead className="font-semibold min-w-[180px] max-w-[220px]">
+              <TableHead className="font-semibold min-w-[180px] max-w-[220px]  pl-12">
                 Query
               </TableHead>
               <TableHead className="w-44 font-semibold">
